efc13be683de824e05b88d6ce4a1d7333b78e6c7
XdmriveroeX <dmriveroe@unal.edu.co>
2023-04-19 18:24:17 -0500

    added index file check and run

--- p1-odProgram.c
+++ p1-odProgram.c
@@ -2,54 +2,90 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-// Constantes para los nombres de los programas
-const char *INTERFACE_PROGRAM = "./interface";
-const char *SEARCHER_PROGRAM = "./searcher";
-
-// Prototipos de funciones
-pid_t create_and_run_child_process(const char *program_name);
-void wait_for_child_processes(pid_t *pids, int num_pids);
-
-
+// Function prototypes
+void execute_interface(void);
+void execute_searcher(void);
+void wait_for_children(pid_t pid1, pid_t pid2);
+void execute_create_indexed_file(void);
 
 int main() {
-    // Crear procesos hijo para la interfaz y el buscador
-    pid_t pid1 = create_and_run_child_process(INTERFACE_PROGRAM);
-    pid_t pid2 = create_and_run_child_process(SEARCHER_PROGRAM);
+    struct stat buffer;
 
-    // Esperar a que ambos procesos hijo terminen
-    pid_t child_pids[] = {pid1, pid2};
-    wait_for_child_processes(child_pids, 2);
+    // Check if the indexed_file.bin doesn't exist
+    if (stat("index_file.bin", &buffer) != 0) {
+        execute_create_indexed_file();
+    }
 
-    printf("Both programs have finished executing.\n");
+    int child_status;
+    pid_t pid1 = fork();
+
+    if (pid1 == 0) {
+        // Child process for interface
+        execute_interface();
+    } else if (pid1 > 0) {
+        pid_t pid2 = fork();
+
+        if (pid2 == 0) {
+            // Child process for searcher
+            execute_searcher();
+        } else if (pid2 > 0) {
+            // Parent process waits for both child processes to finish
+            wait_for_children(pid1, pid2);
+            printf("Both programs have finished executing.\n");
+        } else {
+            perror("Error forking searcher process");
+            exit(1);
+        }
+    } else {
+        perror("Error forking interface process");
+        exit(1);
+    }
 
     return 0;
 }
 
-pid_t create_and_run_child_process(const char *program_name) {
+// Function implementations
+
+void execute_interface() {
+    char *args[] = {"./interface", NULL};
+    if (execvp(args[0], args) == -1) {
+        perror("Error executing interface");
+        exit(1);
+    }
+}
+
+void execute_searcher() {
+    char *args[] = {"./searcher", NULL};
+    if (execvp(args[0], args) == -1) {
+        perror("Error executing searcher");
+        exit(1);
+    }
+}
+
+void wait_for_children(pid_t pid1, pid_t pid2) {
+    int child_status;
+    waitpid(pid1, &child_status, 0);
+    waitpid(pid2, &child_status, 0);
+}
+
+void execute_create_indexed_file() {
     pid_t pid = fork();
 
     if (pid == 0) {
-        // Proceso hijo
-        char *args[] = {(char *)program_name, NULL};
+        // Child process for create_indexed_file
+        char *args[] = {"./create_indexed_file", NULL};
         if (execvp(args[0], args) == -1) {
-            perror("Error executing child process");
+            perror("Error executing create_indexed_file");
             exit(1);
         }
-    } else if (pid < 0) {
-        perror("Error forking child process");
+    } else if (pid > 0) {
+        // Parent process waits for child process to finish
+        int child_status;
+        waitpid(pid, &child_status, 0);
+    } else {
+        perror("Error forking create_indexed_file process");
         exit(1);
     }
-
-    return pid;
 }
-
-void wait_for_child_processes(pid_t *pids, int num_pids) {
-    int child_status;
-
-    for (int i = 0; i < num_pids; i++) {
-        waitpid(pids[i], &child_status, 0);
-    }
-}
-
